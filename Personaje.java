4bbd9cb0ffb06fe9d3b6783c1bb75ca70e9866b2
JoelGutierrez <joelgalumno@gmail.com>
2025-12-05 11:18:17 +0100

    Funcionalidad añadida

--- Personaje.java
+++ Personaje.java
@@ -1,15 +1,27 @@
-public class Personaje { 
-    private String nom; 
-    private int dinero; 
-    public Personaje(String nom, int dinero) { 
-        this.nom = nom; 
-        this.dinero = dinero; 
-    } 
-    public void mostrarInfo() { 
-        System.out.println(nom + " tiene " + dinero + "$."); 
-    } 
-    public static void main(String[] args) { 
-        Personaje jugador = new Personaje("Trevor", 500); 
-        jugador.mostrarInfo(); 
-    } 
+public class Personaje {
+    private String nom;
+    private int dinero;
+
+    public Personaje(String nom, int dinero) {
+        this.nom = nom;
+        this.dinero = dinero;
+    }
+
+    public void mostrarInfo() {
+        System.out.println(nom + " tiene " + dinero + "$.");
+    }
+
+    public void gastarDinero(int cantidad){
+        System.out.println(this.nom + " gasta " + cantidad + "$");
+
+        this.dinero -= cantidad;
+    
+    }
+
+    public static void main(String[] args) {
+        Personaje jugador = new Personaje("Trevor", 500);
+        jugador.mostrarInfo();
+        jugador.gastarDinero(100);
+        jugador.mostrarInfo();
+    }
 }
